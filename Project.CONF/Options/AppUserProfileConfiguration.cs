91aedd945c2f83590bdfbb66758d66fee569e4b6
Ekrem Güngör <ekrmdsgnr@gmail.com>
2024-03-28 10:56:21 +0300

    Configuration başlandı.

---
+++ Project.CONF/Options/AppUserProfileConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.CONF.Options
+{
+    public class AppUserProfileConfiguration : BaseConfiguration<AppUserProfile>
+    {
+        public override void Configure(EntityTypeBuilder<AppUserProfile> builder)
+        {
+            base.Configure(builder);
+
+        }
+    }
+}
---
+++ Project.CONF/Options/BaseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Project.ENTITIES.Interfaces;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.CONF.Options
+{
+    public abstract class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : class, IEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+
+        }
+    }
+}
